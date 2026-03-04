b22927dc7df16fbd8aefdcc7b6848c0228fbb4a7
ayzenman <ayzenman85@gmail.com>
2026-03-04 20:44:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,17 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
-import prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    inspectAttr(),
-    react(),
-    prerender({
-      staticDir: 'dist',
-      routes: ['/'],
-    }),
-  ],
+  plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    ssrManifest: true,
   },
 });
